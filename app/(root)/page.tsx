67cab504567841da8df6ea014b38d9e6e06e4459
Manish <mbb7224@gmail.com>
2023-11-16 11:09:03 +0530

    Fixed bugs on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,39 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions"
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-  const result = await fetchPosts(1, 30)
-  const user = await currentUser()
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-  console.log(result);
-  
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
+
   return (
     <>
-      <h1 className="head-text text-left">Home</h1>      
+      <h1 className='head-text text-left'>Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10">
+      <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
-          <p className="no-result">No Threads found</p>
+          <p className='no-result'>No threads found</p>
         ) : (
           <>
             {result.posts.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user.id || ""}
+                currentUserId={user.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
@@ -35,5 +46,7 @@ export default async function Home() {
         )}
       </section>
     </>
-  )
+  );
 }
+
+export default Home;
